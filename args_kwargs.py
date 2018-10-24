2de2544bfedba5fa751d5537ec50b541f8243997
Saurian-Dash <34033446+Saurian-Dash@users.noreply.github.com>
2018-10-24 21:15:16 +0100

    Add *args and **kwargs

--- args_kwargs.py
+++ args_kwargs.py
@@ -26,3 +26,41 @@ def print_name(first_name, last_name):
 
 print_name(names)   # Will not work
 print_name(**names) # Will work
+
+# Dictionary unpacking becomes very useful when writing control flow
+
+def calculate(**kwargs):
+    # Lookup table of operations
+    operations = {
+        'add':      kwargs.get('first', 0) + kwargs.get('second', 0),
+        'subtract': kwargs.get('first', 0) - kwargs.get('second', 0),
+        'multiply': kwargs.get('first', 0) * kwargs.get('second', 0),
+        'divide':   kwargs.get('first', 0) / kwargs.get('second', 1)
+    }
+    
+    # Perform calculation based on lookup table results
+    calculation = operations[kwargs.get('operation', '')]
+        
+    # Output control flow
+    if kwargs.get('make_float'):
+        return '{} {}'.format(kwargs.get('message', 'The result is'), float(calculation))
+    else:
+        return '{} {}'.format(kwargs.get('message', 'The result is'), int(calculation))
+
+
+# Parameters must be passed in this order:
+# 1. Parameters
+# 2. *args
+# 3. Default parameters
+# 4. **kwargs
+
+calculate(make_float=False, 
+        operation='add',
+        message='You just added',
+        first=2,
+        second=4)   # "You just added 6"
+
+calculate(make_float=True, 
+        operation='divide', 
+        first=3.5, 
+        second=5)   # "The result is 0.7"
