358b1b77b5222ceec10817f4ed51d818f462cfe9
Saurian-Dash <34033446+Saurian-Dash@users.noreply.github.com>
2018-10-25 17:36:18 +0100

    Update control_flow.py

--- control_flow.py
+++ control_flow.py
@@ -2,8 +2,6 @@
 # You can reduce the footprint of if statements using the following syntax:
 
 # Long:
-
-
 def long_age_check(age):
 
     if age < 18:
@@ -19,7 +17,7 @@ def short_age_check(age):
 
     message = 'Not eligible' if age < 18 else 'Eligible'
 
-    return set((age, message))
+    return (age, message)
 
 print(short_age_check(32))
 
@@ -27,6 +25,9 @@ print(short_age_check(32))
 # Function Annotation
 # You can expressely declare the data types a function will read and write:
 
-def add_to_number(num: int, by: int = 1) -> int:
+def add_to_number(num: int, by: int = 1) -> tuple:
 
-    return (num, num * by)
+    return (num, num + by)
+
+
+print(add_to_number(1, 2))
